1eb0d2eac73e7f904ecb88270c89182d2ff84fad
unknown <david.kaiser.82@gmail.com>
2018-01-16 23:06:05 +0100

    worte a README file, change example

---
+++ README.md
@@ -0,0 +1,61 @@
+README
+================
+David Kaiser
+2018/01/16
+
+create a TS diagram as a ggplot
+-------------------------------
+
+### Description
+
+A TS diagram, Temperature-Salinity-diagram, plots the **potential temperature** of water over **salinity**. Many water masses have characteristic shapes in a TS diagram, which is used in physical oceanography to identify water masses and their mixing (see e.g. <https://doi.org/10.1016/S0422-9894(08)71172-3>).
+
+This function requires the input of vectors of **potential temperature** and **salinity**. The *gws* package is used to calculate potential density for plotting of isopycnals (contours of the same density). This calculation requires a **reference pressure**, which defaults to 0, the sea surface.
+
+A third vector can optionally be supplied to **col.par** to be plotted in color, and can be named with a string supplied to **col.name**.
+
+### Arguments
+
+*sal* -- vector of salinity values
+
+*pot.temp* -- vector of potential temperature values in degree C
+
+*reference.p = 0* -- reference pressure which was also used to calculate potential temperature
+
+*col.par = NA* -- optional vector of a parameter to be displayed as color of the TS-pairs
+
+*col.name = "col.par"* -- optional name of the "col.par" to be used on the color bar
+
+### Plot
+
+``` r
+example <- read.csv("example_data/example_data.csv")
+
+head(example)
+```
+
+    ##   X depth potential.temperature salinity
+    ## 1 1  1.50                6.1272  13.2087
+    ## 2 2  1.75                6.1245  13.2115
+    ## 3 3  2.00                6.1236  13.2128
+    ## 4 4  2.25                6.1227  13.2126
+    ## 5 5  2.50                6.1229  13.2145
+    ## 6 6  2.75                6.1224  13.2122
+
+The data will be plotted using the *ggplot2* package.
+
+``` r
+ggTS_DK(sal = example$salinity, 
+        pot.temp = example$potential.temperature, 
+        reference.p = 0,
+        col.par = example$depth, 
+        col.name = "depth [m]")
+```
+
+![](README_files/figure-markdown_github/pressure-1.png)
+
+**NOTE**: the special "Â" character does not show when the function is used in R, this seems to be a markdown/knitr problem
+
+### in preparation
+
+**Isopycnal labels**: Currently the isopycnals are not labeled in the example. For cases in which the isopycnals run fairly horizontally through the plot and cut through its right side edge, and in which the calculated potential density range is large enough, this works already. But this is not the case for the example data.
--- ggTS_DK.R
+++ ggTS_DK.R
@@ -1,7 +1,5 @@
 #### draws a TS diagram using ggplot2
 # created by David Kaiser
-# in prep:
-# write isopycnal values on the top as well (currently only on the right)
 
 # arguments
 ggTS_DK <- function(
@@ -13,8 +11,8 @@ ggTS_DK <- function(
   ) 
   {
   # packages
-  require(gsw)
-  require(ggplot2)
+  library(gsw)
+  library(ggplot2)
   
   # make TS long table
   TS <- expand.grid(
@@ -23,21 +21,24 @@ ggTS_DK <- function(
   )
   TS$density <- gsw_rho_t_exact(SA = TS$sal, t = TS$pot.temp, p = reference.p) - 1000 # the function calculates in-situ density, but because potential temperature and a single reference pressure is used the result equals potential density at reference pressure
   
-  # isopycnal lables
-  isopycnals <- subset(TS,
-                       sal == ceiling(max(TS$sal)) & # selects all rows where "sal" is the max limit of the x axis
-                       round(density,1) %in% seq(min(round(TS$density*2)/2), 
-                                       max(round(TS$density*2)/2), 
-                                       by = .5)) # selects any line where the rounded denisty is equal to density represented by any isopycnal in the plot
-  isopycnals$density <- round(isopycnals$density, 1) # rounds the density
-  isopycnals <- aggregate(pot.temp~density, isopycnals, mean) # reduces number of "pot.temp" values to 1 per each unique "density" value
+  # isopycnal labels 
+  # (works only if the isopycnals cross through the right edge of the plat area ...
+  # ... which is the case for the in the South China Sea, for which this funcion was written,
+  # ... but is not the case in the example data file)
+  #isopycnals <- subset(TS,
+  #                     sal == ceiling(max(TS$sal)) & # selects all rows where "sal" is the max limit of the x axis
+  #                     round(density,1) %in% seq(min(round(TS$density*2)/2), 
+  #                                     max(round(TS$density*2)/2), 
+  #                                     by = .5)) # selects any line where the rounded denisty is equal to density represented by any isopycnal in the plot
+  #isopycnals$density <- round(isopycnals$density, 1) # rounds the density
+  #isopycnals <- aggregate(pot.temp~density, isopycnals, mean) # reduces number of "pot.temp" values to 1 per each unique "density" value
 
   # data
   data <- data.frame(sal, pot.temp, col.par)  
   
   # plot
   p1 <- ggplot() +
-    geom_contour(data = TS,aes(x = sal, y = pot.temp, z = density), col = "grey", linetype = "dashed",
+    geom_contour(data = TS, aes(x = sal, y = pot.temp, z = density), col = "grey", linetype = "dashed",
                  breaks = seq(min(round(TS$density*2)/2), # taking density times 2, rounding and dividing by 2 rounds it to the neares 0.5
                               max(round(TS$density*2)/2), 
                               by = .5)) +
@@ -46,27 +47,17 @@ ggTS_DK <- function(
     geom_path(data = data, aes(sal, pot.temp), color = "black") +
     geom_point(data = data[!is.na(data$col.par),], # plot only the points that have a z value in color according to z
                aes(sal, pot.temp, color = col.par)) +   
-    geom_text(data = isopycnals, 
-              aes(x = ceiling(max(TS$sal)), y = pot.temp, label = density),
-              hjust = "inward", vjust = 0, col = "grey") +
+    #geom_text(data = isopycnals, 
+    #          aes(x = ceiling(max(TS$sal)), y = pot.temp, label = density),
+    #          hjust = "inward", vjust = 0, col = "grey") +
     annotate(geom = "text", x = floor(min(TS$sal)), y = ceiling(max(TS$pot.temp)), 
              hjust = "inward", vjust = "inward", color = "grey60", size = 14,
              label = paste0('sigma',"[",reference.p,"]"), parse = T) +
     scale_x_continuous(name = "salinity", expand = c(0,0), 
                        limits = c(floor(min(TS$sal)), ceiling(max(TS$sal)))) + # use range of "sal" for x axis
-    scale_y_continuous(name = "potential termperature [°C]", 
+    scale_y_continuous(name = "potential temperature [°C]", 
                        limits = c(floor(min(TS$pot.temp)), ceiling(max(TS$pot.temp)))) + # use range of "pot.temp" for y axis
     scale_color_gradientn(colors = c("blue", "green", "yellow", "red"), name = col.name) +
     theme_classic() + theme(text = element_text(size=14))
   return(p1)
 }
-
-# example
-test <- H4_complete[H4_complete$station==41,]
-ggTS_DK(sal = test$sal00, 
-        pot.temp = test$potemp090C, 
-        reference.p = 0, 
-        col.par = test$DOC.µM, 
-        col.name = "depth [m]") 
-
-# Test, if I am allowed to push into the master branch
